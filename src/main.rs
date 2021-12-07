7bc4379474385f0892f73f01c9ea7b5b6a26d5f8
Justin Chu <justinchuby@users.noreply.github.com>
2021-12-06 21:19:51 -0800

    Add main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, BufWriter};
+use std::env;
+use std::process;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => { run_prompt(); }
+        2 => { run_file(&args[1]); }
+        _ => {
+            println!("Usage: rustlox [script]");
+            process::exit(64);
+        }
+    }
 }
