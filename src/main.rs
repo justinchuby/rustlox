3ab251c707abf634a394d87a4093c4644bb1bb12
Justin Chu <justinchuby@users.noreply.github.com>
2021-12-06 22:42:07 -0800

    Add main functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
-use std::io::{stdout, BufWriter};
+use std::io;
 use std::env;
 use std::process;
+
+struct Lox {}
+
+impl Lox {
+    fn run_file(&mut self, path: &str) {
+        let content = std::fs::read_to_string(path).expect("File not found");
+        run(&content);
+    }
+
+    fn run_prompt(&mut self) {
+        loop {
+            print!("> ");
+            let mut line = String::new();
+            match io::stdin().read_line(&mut line) {
+                Ok(n) => {
+                    match n {
+                        0 => { break; }
+                        _ => { run(line) }
+                    }
+                }
+                Err(error) => {
+                    println!("error: {}", error);
+                    break;
+                }
+            }
+        }
+    }
+
+    fn run(&mut self, source: &str) {
+        let scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens();
+
+        for token in tokens.iter() {
+            println!(token)
+        }
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
